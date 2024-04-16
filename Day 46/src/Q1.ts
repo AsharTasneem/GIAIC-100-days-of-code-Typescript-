0fa883b01d88796a30782d4a14846683babbc2fc
Ashar Tasneem <133583260+AsharTasneem@users.noreply.github.com>
2024-04-16 15:26:59 +0500

    Day 46

---
+++ Day 46/src/Q1.ts
@@ -0,0 +1,10 @@
+#! /usr/bin/env node.
+// Use console.log() to debug and track the value of a variable inside a loop.
+
+// Explain & TIP: console.log() is an invaluable tool for developers to monitor the values of variables, the flow of execution, and to understand the state of their program at various points in time, especially within loops where values change with each iteration.
+
+// Demonstrates using console.log() inside a loop to track variable values
+for (let i = 1; i <= 5; i++) {
+    console.log(`Iteration ${i}, i value:, i`); // Logs the current iteration number and the value of i
+  }
+  // This loop iterates five times, logging the value of 'i' during each iteration to help with debugging.
---
+++ Day 46/src/Q2.ts
@@ -0,0 +1,10 @@
+#! /usr/bin/env node.
+// The try-catch statement marks a block of statements to try, and specifies a response, should an exception be thrown. It's a critical feature for robust error handling in JavaScript.
+
+// Answer: Please try to do it yourself first!
+
+try {
+  console.log(a);
+} catch (error) {
+  console.log("a is not defined.");
+}
