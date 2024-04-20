25af76dfe0adc868cc7721ec30db853f9e4d81a4
Ashar Tasneem <133583260+AsharTasneem@users.noreply.github.com>
2024-04-20 14:53:01 +0500

    Day 50

---
+++ Day 50/bin/Q1.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+//  Demonstrate the use of the setTimeout() function to execute code after a delay.
+// Explain & TIP: setTimeout() is a built-in JavaScript function that allows you to execute a piece of code after a specified delay. It's especially useful for scheduling tasks, animations, or delaying execution without blocking the main thread.
+setTimeout(() => { console.log("Hi Ashar"); }, 2000);
+export {};
---
+++ Day 50/bin/Q2.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+// Explain the concept of the event loop in JavaScript with an example.
+// Explain & TIP: The event loop is a fundamental concept in JavaScript, enabling asynchronous operations. It monitors the call stack and the callback queue. If the call stack is empty, it moves the first event from the queue to the stack, /ensuring non-blocking execution.
+function delay() {
+    console.log("Start");
+    setTimeout(() => {
+        console.log("Hello World");
+    }, 3000);
+    console.log("End");
+}
+delay();
+export {};
